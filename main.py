b3e2268fa6c3f501d39e4376a2429fbe975a547a
Ari <arianne.tran@tum.de>
2022-02-11 03:44:38 +0100

    running segmentation and saving script which also saves plot for visual inspection

--- main.py
+++ main.py
@@ -3,53 +3,18 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import pandas as pd
-import os.path
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.io import wavfile
-from batch_segmentation import *
+
+from workstationfile import return_data_locs
+from preprocess import preprocessing
 
 from pathlib import Path
 
+def main():
+    # 1 for großhadern, 2 for home, 3 for laptop, 4 for server
+    filePath, labelPath, savePath = return_data_locs(2)
 
-filePath = '/media/ari/Harddisk/knee data/knee signal/Patienten/'
-labelPath = '/media/ari/Harddisk/knee data/knee signal/'
-# general testing if file exists
-directory = Path('/media/ari/Harddisk/knee data/knee signal/')
-isit = directory.is_dir()
-file_there = os.path.isfile(labelPath + 'Übersicht Probanden_Patienten_Upload.xlsx')
+    # preprocess data and labels, splitting into segments and saving, or simple loading from split location
+    preprocessing(filePath, labelPath, savePath)
 
-# read in labels
-Knee_label_list = pd.read_excel(labelPath + 'Übersicht Probanden_Patienten_Upload.xlsx')
-
-
-# read in data
-session_list = os.listdir(filePath)
-patient_files = os.listdir(filePath + session_list[0])
-approved = ["wav"]
-
-# filter out all the wav files and sort by sensor (2 runs/2 files)
-patient_files[:] = [w for w in patient_files if any(sub in w for sub in approved)]
-patella_data = [s for s in patient_files if 'Patella' in s]
-tibiaMedial_data = [s for s in patient_files if 'Medial' in s]
-tibiaLateral_data = [s for s in patient_files if 'Lateral' in s]
-
-samplerate, bone_music  = wavfile.read(filePath + session_list[0] + '/' + patient_files[0])
-# test plotting
-bone_sound = bone_music .shape[0]
-angles = bone_music [:, 1]
-
-length = bone_sound / samplerate
-time = np.linspace(0., length, bone_music .shape[0])
-plt.plot(time, angles, label="left channel")
-plt.legend()
-plt.xlabel("Time in seconds")
-plt.ylabel('Amplitude')
-plt.show()
-breaking = 0
-
-sections = segmentation_jhu(samplerate, )
-
-# preprocess (read in all data, save in structure, split in extension flexion cycle)
-
+if __name__ == '__main__':
+    main()
--- preprocess.py
+++ preprocess.py
@@ -4,73 +4,126 @@ import os.path
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from vaghelpers import vag2float
-from workstationfile import return_data_locs
 from batch_segmentation import *
 from pathlib import Path
-
-# 1 for großhadern, 2 for home, 3 for laptop, 4 for server
-filePath, labelPath, savepath = return_data_locs(2)
-
-# read in all labels
-Knee_label_list = pd.read_excel(labelPath + 'Übersicht Probanden_Patienten_Upload.xlsx')
-
-# read in all data location
-session_list = os.listdir(filePath)
+import numpy as np
+import matplotlib.lines as mlines
 
 
-for session in range(len(session_list)):
-    patient_files = os.listdir(filePath + session_list[session])
-    approved = ["wav"]
+def newline(p1, p2):
+    ax = plt.gca()
+    xmin, xmax = ax.get_xbound()
 
-    # filter out all the wav files and sort by sensor (2 runs/2 files)
-    patient_files[:] = [w for w in patient_files if any(sub in w for sub in approved)]
-    patella_data = [s for s in patient_files if 'Patella' in s]
-    tibiaMedial_data = [s for s in patient_files if 'Medial' in s]
-    tibiaLateral_data = [s for s in patient_files if 'Lateral' in s]
+    if p2[0] == p1[0]:
+        xmin = xmax = p1[0]
+        ymin, ymax = ax.get_ybound()
+    else:
+        ymax = p1[1]+(p2[1]-p1[1])/(p2[0]-p1[0])*(xmax-p1[0])
+        ymin = p1[1]+(p2[1]-p1[1])/(p2[0]-p1[0])*(xmin-p1[0])
 
-    samplerate, bone_music = wavfile.read(filePath + session_list[session] + '\\' + patient_files[session])
-
-    bone_music  = vag2float(bone_music , np.float32)
-
-    signal = bone_music [:, 0]
-    angles = bone_music [:, 1]
-
-    # test plotting
-    length = bone_music .shape[0] / samplerate
-    time = np.linspace(0., length, bone_music .shape[0])
-
-    fig, axs = plt.subplots(2)
-    axs[0].plot(time, angles, label="right channel")
-    axs[0].legend()
-    plt.xlabel("Time in seconds")
-    plt.ylabel('Amplitude')
-    axs[1].plot(time, signal, label="left channel")
-    axs[1].legend()
-    plt.xlabel("Time in seconds")
-    plt.ylabel('Amplitude')
-    plt.show()
-    breaking = 0
-
-    # todo write automation script and seperation script, plot visuals, pre-process audio data, send patrick data and paper, read some stuff on instrument or speech distinction, learn
-    # todo how to do transfer learning and try with pre-trained networks, try the matlab code to distringuish between healthy and sick patients, look and weights and biases
-    # run the entire code from nima and walther on their data again, try on ours
-
-    # bone_music  = preprocess(bone_music )  # return divided section from a single recording session
-    sections = segmentation_jhu(samplerate, angles)
-
-    # maybe save as single sequences for faster read in
-
-    XSEGMENTS = numpy.sort(sections)
-    XSEGMENTS = numpy.reshape(XSEGMENTS, (-1, 2))
-    output_filepath = askdirectory(parent=root)
-    output_filepath = os.path.join(os.path.normcase(output_filepath), realname)
-    for i in range(0, len(XSEGMENTS)):
-        wavfile.write(output_filepath + "_segment_" + str(i + 1) + ".wav", samplerate, bone_music [XSEGMENTS[i][0]:XSEGMENTS[i][1]])
-    print(u"ExportToFiles ... Finish!")
+    l = mlines.Line2D([xmin, xmax], [ymin, ymax])
+    ax.add_line(l)
+    return l
 
 
-    #create path if it does not exist yet
-    Path("/my/directory").mkdir(parents=True, exist_ok=True)
-    
-    # preprocess (read in all data, save in structure, split in extension flexion cycle)
+def preprocessing(filePath, labelPath, savePath):
+
+    # read in all labels
+    #todo save labels per file for easier access
+    Knee_label_list = pd.read_excel(labelPath + 'Übersicht Probanden_Patienten_Upload.xlsx')
+
+    # read in all data location
+    session_list = os.listdir(filePath)
+
+    for session in range(len(session_list)):
+        patient_files = os.listdir(filePath + session_list[session])
+        approved = ["wav"]
+
+        # filter out all the wav files and sort by sensor (2 runs/2 files)
+        patient_files[:] = [w for w in patient_files if any(sub in w for sub in approved)]
+        patella_data = [s for s in patient_files if 'Patella' in s]
+        tibiaMedial_data = [s for s in patient_files if 'Medial' in s]
+        tibiaLateral_data = [s for s in patient_files if 'Lateral' in s]
+
+        for soundfile in range(len(patient_files)):
+            samplerate, bone_music = wavfile.read(filePath + session_list[session] + '\\' + patient_files[soundfile])
+            realname = patient_files[soundfile].replace(".wav", "")
+
+            bone_music = vag2float(bone_music, np.float32)
+
+            signal = bone_music[:, 0]
+            angles = bone_music[:, 1]
+
+            #  todo  pre-process audio data, send patrick data
+            #   and paper, read some stuff on instrument or speech distinction, learn
+            #  todo how to do transfer learning and try with pre-trained networks, try the matlab code to distringuish
+            #   between healthy and sick patients, look and weights and biases
+            #   run the entire code from nima and walther on their data again, try on ours
+
+            # preprocess (read in all data, save in structure, split in extension flexion cycle)
+            # todo add segmentation into flexion and extension? but words would be even shorter
+            # bone_music  = preprocess(bone_music )  # return divided section from a single recording session
+            sections = segmentation_jhu(samplerate, angles)
+
+            # test plotting
+            length = bone_music.shape[0] / samplerate
+            time = np.linspace(0., length, bone_music.shape[0])
+
+            even = tuple(time[sections[0:len(sections):2]])
+            uneven = tuple(time[sections[1:len(sections):2]])
+
+            fig, axs = plt.subplots(2)
+            axs[0].plot(time, angles)
+            axs[0].vlines(even, min(angles), max(angles), linestyles='dotted', colors='green')  # plot start of segment
+            axs[0].vlines(uneven, min(angles), max(angles), linestyles='dotted', colors='red')  # plot end of segment
+            # plot start of segment
+            plt.xlabel("Time in seconds")
+            plt.ylabel('Amplitude')
+            axs[1].plot(time, signal)
+            plt.xlabel("Time in seconds")
+            plt.ylabel('Amplitude')
+            plt.savefig(savePath + realname + '.png')
+            plt.close()
+            #plt.show()
+
+            # maybe save as single sequences for faster read in
+            x_segments = np.reshape(np.sort(sections), (-1, 2))
+
+            # create path if it does not exist yet
+            if session == 0:
+                Path(savePath).mkdir(parents=True, exist_ok=True)
+
+            for i in range(0, len(x_segments)):
+                # extract the single movement and identify which sensor it came from
+                single_movement = bone_music[:, 0]
+                single_movement = single_movement[x_segments[i][0]:x_segments[i][1]]
+                sensortype = ''
+
+                if 'Medial' in realname:
+                    sensortype = 'Medial'
+                elif 'Lateral' in realname:
+                    sensortype = 'Lateral'
+                else:
+                    sensortype = 'Patella'
+
+                # test plotting
+                length = single_movement.shape[0] / samplerate
+                time = np.linspace(0., length, single_movement.shape[0])
+
+                plt.title(sensortype + " sensor")
+                plt.xlabel("amplitude")
+                plt.ylabel("time in seconds")
+                plt.plot(time, single_movement)
+                plt.savefig(savePath + realname + "_segment_" + str(i + 1) + '.png')
+                plt.close()
+                #plt.show()
+
+                wavfile.write(savePath + realname + "_segment_" + str(i + 1) + ".wav", samplerate, single_movement)
+
+        print(u"Session" +str(session) + "ExportToFiles ... Finish!")
+
+
+
+
+
 
